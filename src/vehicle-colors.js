1515402596b47259e2b0e66880d9bb552a01ffff
Manish Khedekar <mkhedekar@equalexperts.com>
2020-02-14 08:38:52 +0000

    Invalid colors removed

--- src/vehicle-colors.js
+++ src/vehicle-colors.js
@@ -2,17 +2,10 @@ export const VehicleColors = [
     'black',
     'blue',
     'cornflowerblue',
-    'cyan',
-    'darkgreen',
     'gray',
-    'green',
     'tomato',
-    'orange',
-    'pink',
     'purple',
     'red',
-    'skyblue',
-    'violet',
     'white',
     'yellow'
   ];
