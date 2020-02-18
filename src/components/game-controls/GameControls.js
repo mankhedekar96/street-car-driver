d40bed8e2960e3eb20e8f2dfa6ca3a178aa595c3
Manish Khedekar <mkhedekar@equalexperts.com>
2020-02-18 20:30:32 +0000

    Audio Compressed

--- src/components/game-controls/GameControls.js
+++ src/components/game-controls/GameControls.js
@@ -3,18 +3,30 @@ import "./GameControls.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default class GameMenu extends React.Component {
+
+  touches = {};
+
   constructor(props) {
     super(props);
   }
 
   handleTouchStart = (event, key) => {
-      event.key = key;
-      this.props.keyUp(event);
+      this.touches[key] = setInterval(()=>{
+        event.key = key;
+        this.props.keyUp(event);
+      },100);
+      console.log("Touch Start", this.touches);
+
+      return false;
   }
 
   handleTouchEnd = (event, key) => {
+    clearInterval(this.touches[key]);
     event.key = key;
     this.props.keyDown(event);
+    console.log("Touch END", this.touches);
+
+    return false;
   }
 
   render() {
@@ -34,18 +46,18 @@ export default class GameMenu extends React.Component {
         {!this.props.gameOver && this.props.gameStarted && !this.props.gamePaused && (
           <>
             <div className="p-abs p-bottom-left">
-              <button className="button" onTouchStart={ (e) => this.handleTouchStart(e, "ArrowUp") } onTouchEnd={ (e) => this.handleTouchEnd(e, "ArrowUp") }>
+              <button className="button" onTouchStart={ (e) => { return this.handleTouchStart(e, "ArrowUp");} } onTouchEnd={ (e) => { return this.handleTouchEnd(e, "ArrowUp");} }>
                 <FontAwesomeIcon icon="space-shuttle" />
               </button>
-              <button className="button" onTouchStart={ (e) => this.handleTouchStart(e, "ArrowDown") } onTouchEnd={ (e) => this.handleTouchEnd(e, "ArrowDown") }>
+              <button className="button" onTouchStart={ (e) => { return this.handleTouchStart(e, "ArrowDown");} } onTouchEnd={ (e) => { return this.handleTouchEnd(e, "ArrowDown");} }>
                 <FontAwesomeIcon icon="wind" />
               </button>
             </div>
             <div className="p-abs p-bottom-right">
-              <button className="button" onTouchStart={ (e) => this.handleTouchStart(e, "ArrowLeft") } onTouchEnd={ (e) => this.handleTouchEnd(e, "ArrowLeft") }>
+              <button className="button" onTouchStart={ (e) => { return this.handleTouchStart(e, "ArrowLeft");} } onTouchEnd={ (e) => { return this.handleTouchEnd(e, "ArrowLeft");} }>
                 <FontAwesomeIcon icon="arrow-alt-circle-left" />
               </button>
-              <button className="button" onTouchStart={ (e) => this.handleTouchStart(e, "ArrowRight") } onTouchEnd={ (e) => this.handleTouchEnd(e, "ArrowRight") }>
+              <button className="button" onTouchStart={ (e) => { return this.handleTouchStart(e, "ArrowRight");} } onTouchEnd={ (e) => { return this.handleTouchEnd(e, "ArrowRight");} }>
                 <FontAwesomeIcon icon="arrow-alt-circle-right" />
               </button>
             </div>
